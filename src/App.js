7975612478287b8c1a174862bc14e1c46f4306b5
Jessie He <jessie.j.he@accenture.com>
2020-09-13 10:47:11 +1000

    Set up new react project, and install react-redux, material ui, and styled component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Filter from "components/Filter";
+import Products from "components/Products";
+
+function App() {
+  return (
+    <div>
+      <Filter />
+      <Products />
+    </div>
+  );
+}
+
+export default App;
